b3e20df50509be961c41fc085b6d489b780929ed
Yasol Jeong <74160217+yasol123@users.noreply.github.com>
2021-05-20 23:16:41 -0400

    Initial Commit

---
+++ hash_map_example.java
@@ -0,0 +1,18 @@
+package maps_example;
+
+import java.util.HashMap;
+
+public class TestHashMap {
+	public static void main(String[] args) {
+		//HashMap created,HashMap<key, val>, key:integer, val:String 
+		HashMap<Integer, String> myMap = new HashMap<>();
+		//"put" method to add elements to our HashMap.
+		myMap.put(0, "James");
+		myMap.put(1, "Mike");
+		myMap.put(2, "Mary");
+		//uses "get" method to retrieve a value stored at a location set by the key
+		for (int i = 0;i<myMap.size();i++) {
+			System.out.println("The String stored under the key "+i+" is: " + myMap.get(3));
+		}
+	}
+}
