5ce4794398ab8ccb288832315531589efb0a4ef5
Yasol Jeong <74160217+yasol123@users.noreply.github.com>
2021-05-20 23:18:11 -0400

    Update hash_map_example.java

--- hash_map_example.java
+++ hash_map_example.java
@@ -4,6 +4,12 @@ import java.util.HashMap;
 
 public class TestHashMap {
 	public static void main(String[] args) {
+		/*
+		*Input: (0, "James"), (1, "Mike"), (2, "Mary")
+		*Output: The String stored under the key 0 is: null
+		*	 The String stored under the key 1 is: null
+		*	 The String stored under the key 2 is: null
+		*/
 		//HashMap created,HashMap<key, val>, key:integer, val:String 
 		HashMap<Integer, String> myMap = new HashMap<>();
 		//"put" method to add elements to our HashMap.
